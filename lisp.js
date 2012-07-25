760567a3c14d6b4733be75d9e080dfa3e8a8fd0a
Bodil Stokke <bodil@bodil.tv>
2012-07-25 21:19:27 +0200

    Run scripts from command line.

--- lisp.js
+++ lisp.js
@@ -1,6 +1,25 @@
+var fs = require("fs");
 var types = require("./types");
 var load = require("./load");
 var rt = require("./rt")();
+
+var argv = require("optimist").check(function(argv) {
+    if (argv._.length > 1) throw "One file at a time!";
+}).usage("Run you a lisp!\nUsage: $0 [source-file]").argv;
+
+if (argv._.length > 0) {
+    var ast = load(fs.readFileSync(argv._[0], "utf-8"));
+    for (var i = 0, l = ast.length, stm = ast[0]; i < l; stm = ast[++i]) {
+        try {
+            rt.exec(stm);
+        } catch (e) {
+            process.stdout.write("*** In form: " + types.pprint(stm) +
+                                 "\n*** " + e + "\n");
+            process.exit(1);
+        }
+    }
+    process.exit(0);
+}
 
 process.stdout.write(">>> ");
 process.stdin.resume();
